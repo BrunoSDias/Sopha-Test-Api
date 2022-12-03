cba65e0e82bf871c2cc3453d355c2683254f2e43
Lucemila Bonifácio <lucemaylabb@gmail.com>
2022-12-02 21:37:41 -0300

    Feat: adiciona validates

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,5 @@
 class Store < ApplicationRecord
   belongs_to :user
+
+  validades :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :validatable
-                    #:confirmable, :omniauthable, :trackable, 
-            include DeviseTokenAuth::Concerns::User
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+  #:confirmable, :omniauthable, :trackable, 
+  include DeviseTokenAuth::Concerns::User
+
+  validades :email, uniquess: true
+  validades :email, :name, :password, presence: true
 end

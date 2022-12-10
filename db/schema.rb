b745e2605f73850a55b89c5d4014aaf2305368a1
danlongname <pro.danilorps@gmail.com>
2022-12-10 19:39:05 -0300

    added uniquenes to user email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221210164844) do
+ActiveRecord::Schema.define(version: 20221210222608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20221210164844) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

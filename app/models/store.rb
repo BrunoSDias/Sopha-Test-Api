733ab94af99704923b2529f10409b0794a07309e
Kanamy Stewart <90398386+KanamyDamiao@users.noreply.github.com>
2022-12-20 20:37:21 -0300

    Revert "Criar api para catálogos"

--- app/models/store.rb
+++
@@ -1,4 +0,0 @@
-class Store < ApplicationRecord
-    validates :user_id, presence: true
-    belongs_to :user
-end
--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :password, presence: true
-    has_many :stores
-end

a447d2fdcf28253c558704f6155c0a284aa68c12
KanamyDamiao <kanamystewart@gmail.com>
2022-12-23 17:52:10 -0300

    testes de request

--- spec/requests/api/user_spec.rb
+++
@@ -1,9 +0,0 @@
-# require 'rails_helper'
-
-# RSpec.describe 'User API', type: :request do
-#     let(:user) { create(:user) }
-#     let(:headers) { valid_headers.except('Authorization') }
-#     let(:valid_attributes) do
-#         attributes_for(:user, password_confirmation: user.password)
-#     end
-# end

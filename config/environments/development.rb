b4b6cd6206d840282d29c443c60b3be170bbb341
Joao Almeida <aalmeidadev@gmail.com>
2023-09-21 14:56:29 -0300

    Configurando devise e letter_opener para enviroments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  # devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # devise
+  # config.action_mailer.default_url_options = { host: 'www.meudominio.com', port: 80 }
 end

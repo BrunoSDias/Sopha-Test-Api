a788b14c9b439681719143887b94fc55f13fec64
Jonathan <jonathan.maglianorx@gmail.com>
2023-09-21 16:28:16 -0300

    Updates

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server 'localhost', user: 'deploy', roles: %w{web app db}
+server 'https://ec2-35-89-204-246.us-west-2.compute.amazonaws.com:3000/', user: 'deploy', roles: %w{web app db}
 set :rails_env, 'production'
